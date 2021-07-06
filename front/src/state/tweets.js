efdafa86252e9234a9a13ae52d8cedd5455d5682
Diego <diego-jofre@live.com.ar>
2021-07-06 13:24:07 -0300

    update tweets reducer

--- front/src/state/tweets.js
+++ front/src/state/tweets.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getTweetsRequest = createAsyncThunk("TWEETS", () => {
-  return axios.get("/api/tweets").then((r) => r.data);
+  return axios.get("/api/tweets").then((r) => {
+    console.log(r.data);
+    return r.data;
+  });
 });
 
 export const getSingleTweetRequest = createAsyncThunk("TWEET", (id) => {
@@ -18,7 +21,6 @@ export const getUserTweetsRequest = createAsyncThunk("USER_TWEETS", (name) => {
 });
 
 export const postTweetRequest = createAsyncThunk("CREATE_TWEET", (args) => {
-  console.log("ARGS", args);
   return axios
     .post('/api/tweets/', args)
     .then((r) => r.data);
@@ -27,21 +29,15 @@ export const postTweetRequest = createAsyncThunk("CREATE_TWEET", (args) => {
 export const deleteTweetRequest = createAsyncThunk("DELETE_TWEET", (id) => {
   return axios
     .delete(`/api/tweets/${id}`)
-    .then((r) => r.data[0])
-});
-
-export const updateUserTweetsRequest = createAsyncThunk("UPDATE_USER_TWEETS", (args) => {
-  return axios
-    .put(`/api/users/${args.name}`, { isVerified: args.isVerified })
+    .then((r) => r.data)
 });
 
 const tweetsReducer = createReducer([], {
   [getTweetsRequest.fulfilled]: (state, action) => action.payload,
-  [getSingleTweetRequest.fulfilled]: (state, action) => action.payload,
+  [getSingleTweetRequest.fulfilled]: (state, action) => [action.payload],
   [getUserTweetsRequest.fulfilled]: (state, action) => action.payload,
   [postTweetRequest.fulfilled]: (state, action) => [...state, action.payload],
   [deleteTweetRequest.fulfilled]: (state, action) => state.filter(tweet => tweet.id !== action.payload.id),
-  [updateUserTweetsRequest.fulfilled]: (state, action) => [...state],
 });
 
 export default tweetsReducer;
